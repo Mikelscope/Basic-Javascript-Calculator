c204245e20a56c29a6f76af5dc51aac30cd9d1ba
Michael Tejiri <105463230+Mikelscope@users.noreply.github.com>
2022-06-25 14:38:33 +0100

    Add files via upload

---
+++ calcConsole.js
@@ -0,0 +1,22 @@
+alert('My Calculator'  +  '\nPress OK to proceed' )
+let num1 = parseInt(prompt('Number 1: '));
+let opr = prompt('Operator: ');
+let num2 = parseInt(prompt('Number 2: '));
+let numAdd = num1 + num2;
+let numMinus = num1 - num2;
+let numTimes = num1 * num2;
+let numDivide = num1 / num2;
+
+if(opr == '+')
+{
+    alert( 'Result is ' + numAdd)
+}else if(opr == '-')
+{
+    alert('Result is ' + numMinus)
+}else if(opr == '*')
+{
+    alert('Result is ' + numTimes)
+}else if(opr == '/')
+{
+    alert('Result is ' + numDivide)
+}
